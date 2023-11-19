d4b25b5685082d006a52581388d2c541e7ea29b9
gaellucero <gael@udel.edu>
2023-11-18 20:11:12 -0500

    Created the zombies, made them spawn randomly and when cross the line the game pauses

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
 from designer import *
+from random import randint
 INITIAL_SPEED = 0
 HERO_SPEED = 5
 LASER_SPEED = 5
+ZOMBIE_SPEED = 5
 @dataclass
 class World:
     hero: DesignerObject
@@ -20,6 +22,7 @@ def create_hero() -> DesignerObject:
     hero.y = get_height() * (1/2)
     hero.x = get_width() * (1/8)
     hero.flip_x = True
+    print(hero.x)
     return hero
 
 def move_hero(world: World):
@@ -62,7 +65,7 @@ def make_laser_fly(world: World):
         laser.x += LASER_SPEED
 
 def destroy_lasers_offscreen(world: World):
-    """ Destroy any lasers that have landed on the ground """
+    """ Destroy any lasers that are offscreen """
     kept = []
     for laser in world.lasers:
         if laser.x < get_width():
@@ -70,6 +73,41 @@ def destroy_lasers_offscreen(world: World):
         else:
             destroy(laser)
     world.lasers = kept
+
+def create_zomie() -> DesignerObject:
+    """ Create a zombie randomly on the right-side of the screen """
+    zombie = emoji("👻")
+    zombie.scale_x = 1
+    zombie.scale_y = 1
+    zombie.anchor = 'midbottom'
+    zombie.x = get_width()
+    zombie.y = randint(0, get_height())
+    return zombie
+
+def make_zombies(world: World):
+    """ Create a new zombie at random times, if there aren't enough zombies """
+    random_chance = randint(1, 10) == 1
+    if len(world.zombies) < 10 and random_chance:
+        world.zombies.append(create_zomie())
+
+def zombie_run(world: World):
+    """ Move all the zombies to the left """
+    for zombie in world.zombies:
+        zombie.x -= ZOMBIE_SPEED
+
+def speedup_zombies(world: World):
+    """ Make each zombie get a little bit faster """
+    for zombie in world.zombies:
+        zombie.x -= .01
+        #zombie.scale_y += .01
+
+def zombies_cross_the_line(world: World) -> bool:
+    """ Return True if there are any zombies that cross the line """
+    for zombie in world.zombies:
+        if zombie.x <= 100:
+            return True
+    return False
+
 
 
 when('starting', create_world)
@@ -79,4 +117,8 @@ when("typing", control_hero)
 when('typing', shoot_laser)
 when("updating", make_laser_fly)
 when("updating", destroy_lasers_offscreen)
+when("updating", make_zombies)
+when("updating", zombie_run)
+#when("updating", speedup_zombies)
+when(zombies_cross_the_line, pause)
 start()
